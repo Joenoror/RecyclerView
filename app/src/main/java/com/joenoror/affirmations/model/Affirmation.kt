84969ce62cb10530438dbfaabe1185e9ebf63bac
Joenoror <falanhx@gmail.com>
2022-05-06 12:35:33 +0200

    Add images and icon app

--- app/src/main/java/com/joenoror/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/joenoror/affirmations/model/Affirmation.kt
@@ -1,3 +1,9 @@
 package com.joenoror.affirmations.model
 
-data class Affirmation(val stringResourceId: Int)
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import java.util.*
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int)
